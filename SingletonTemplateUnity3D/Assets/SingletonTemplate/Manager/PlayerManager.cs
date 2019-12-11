41eea0f9c1a01c4dc9e40f30177aab4af65610df
jeelpatelofficial <jeelpatel8460553028@gmail.com>
2019-12-11 16:19:27 -0500

    Initial commit

---
+++ SingletonTemplateUnity3D/Assets/SingletonTemplate/Manager/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : IManager
+{
+    void IManager.Initialize()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void IManager.PhysicsRefresh()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void IManager.PostInitialize()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void IManager.Refresh()
+    {
+        throw new System.NotImplementedException();
+    }
+}
